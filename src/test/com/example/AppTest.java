f7afbee0dc4732d8a0e7fdc21b75027f13d70be1
Tantely61123 <150002629+Tantely61123@users.noreply.github.com>
2025-07-23 10:49:38 +0200

    Update AppTest.java

--- src/test/com/example/AppTest.java
+++ src/test/com/example/AppTest.java
@@ -2,10 +2,12 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
     public void testApp() {
+        assertTrue(true);
         assertEquals(1, 1);
     }
 }
